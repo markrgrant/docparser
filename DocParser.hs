6ceb1448ee70fff0b3774c0a2eeac17da97b048b
Mark Grant <markrgrant@yahoo.com>
2015-12-24 14:41:12 -0800

    refactoring

--- DocParser.hs
+++ DocParser.hs
@@ -10,6 +10,9 @@ import Data.Char (isSpace)
 import Doc
 
 
+-- Parses a string containing the document of interest and
+-- returns either a string containing an error message if the
+-- document is not valid, or a Doc value.
 parseDoc :: String -> Either String Doc
 parseDoc str = case parse document "" str of
     Left err -> Left $ showErr err
@@ -22,37 +25,47 @@ parseDoc str = case parse document "" str of
         showLastErr err = messageString $ last $ errorMessages err
 
 
-punctuationChars = "!?."  -- sentence termination characters
-
-
-wordEndChars = " " ++ punctuationChars
-
-
-spacesNoEndOfLine = skipMany (satisfy (\ch -> isSpace ch && ch /= '\n'))
-
-
--- A word is one or more characters that are not punctuation or whitespace.
--- Trailing whitespace is cleared.
-word :: Parser Word
-word = do
-    chars <- many1 (noneOf wordEndChars)
+-- Parses a string into a Doc value.  Leading whitespace is removed
+-- and zero or more paragraphs are parsed until the end of the
+-- document is reached.
+document :: Parser Doc
+document = do
     spaces
-    return $ Word chars
+    paragraphs <- manyTill paragraph documentEnd
+    return $ Doc paragraphs
 
 
-period = char '.' >> return Period
+-- A document ends with optional whitespace.
+documentEnd = spaces >> eof
 
-exclamation = char '!' >> return Exclamation
 
-question = char '?' >> return Question
+-- Parses a string into a paragraph.  There must be no leading
+-- whitespace.   Trailing whitespace is removed, provided it is
+-- either the end of the document or the whitespace is a valid 
+-- paragraph separator (whitespace containing two or more carriage
+-- returns).
+paragraph :: Parser Paragraph
+paragraph = do
+    sentences <- manyTill sentence (try paragraphEnd)
+    return $ Paragraph sentences
 
-punctuation :: Parser Punctuation
-punctuation = try period <|> try exclamation <|> question
+
+paragraphSep =
+    spacesNoEndOfLine >>
+    endOfLine >>
+    spacesNoEndOfLine >>
+    endOfLine >>
+    spaces
+
+
+paragraphEnd = try documentEnd <|> paragraphSep
+
 
 -- A sentence is one or more words followed by a period.  The period could
 -- be separated from the last word in the sentence by zero or more spaces.
--- A sentence ends either with a sentence separator (which is consumed) or 
--- with a paragraph separator (which is not consumed).
+-- A sentence ends either with the end of the paragraph (which is
+-- not consumed), the start of a new sentence (whitespace containing at
+-- most one newline), or whitespace with no newlines.
 sentence :: Parser Sentence
 sentence = do
     words <- many1 word
@@ -62,26 +75,28 @@ sentence = do
 
 
 sentenceEnd = (lookAhead (try paragraphEnd)) <|>
-    try (spacesNoEndOfLine >> endOfLine >> spacesNoEndOfLine) <|> spacesNoEndOfLine
+    try spacesNoEndOfLine <|>
+     spacesNoEndOfLine
+
+sentenceSep = spacesNoEndOfLine >> endOfLine >> spacesNoEndOfLine
+
+spacesNoEndOfLine = skipMany (satisfy (\ch -> isSpace ch && ch /= '\n'))
 
 
--- A paragraph consists of one or more sentences.  Paragraphs are
--- separated from each other by two newlines.
-paragraph :: Parser Paragraph
-paragraph = do
-    sentences <- manyTill sentence (try paragraphEnd)
-    return $ Paragraph sentences
-
-
--- a paragraph separator is two or more newlines occurring within whitespace.
-paragraphEnd = try documentEnd <|> (spacesNoEndOfLine >> endOfLine >> spacesNoEndOfLine  >> endOfLine >> spaces)
-
-
-document :: Parser Doc
-document = do
+-- A word is one or more characters that are not punctuation or whitespace.
+-- Trailing whitespace is cleared.
+word :: Parser Word
+word = do
+    chars <- many1 (noneOf wordEnd)
     spaces
-    paragraphs <- manyTill  paragraph documentEnd
-    return $ Doc paragraphs
+    return $ Word chars
 
+punctuation :: Parser Punctuation
+punctuation = try period <|> try exclamation <|> question
+    where period = char '.' >> return Period
+          exclamation = char '!' >> return Exclamation
+          question = char '?' >> return Question
 
-documentEnd = spaces >> eof
+punctuationChars = "!?."
+
+wordEnd = " " ++ punctuationChars
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # README
 
 A simple text document parser library using parsec.
+
+## Installation
+
+Set up a cabal sandbox
+
+```
+cabal sandbox init
+cabal install
+```
+
+
+## Tests
+
+Run tests using ``cabal test``
